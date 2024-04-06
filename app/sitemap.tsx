c454d6235aef68a141e2b1c61a3834e8bbc6b2c7
udhayasilambam96 <udhayasilambam96@hotmail.com>
2024-04-06 22:07:34 +0530

    Update sitemap.tsx

--- app/sitemap.tsx
+++ app/sitemap.tsx
@@ -8,27 +8,27 @@ export default async function sitemap() {
       changeFrequency: "weekly",
       priority: 1,
     },
-    ...blogs.docs.map((blog: any) => ({
-      url: `https://assessment-nextapp.vercel.app/${blog.slug}`,
-      lastModified: new Date().toISOString(),
-      changeFrequency: "weekly",
-      priority: 0.8,
-    })),
+    // ...blogs.docs.map((blog: any) => ({
+    //   url: `https://assessment-nextapp.vercel.app/${blog.slug}`,
+    //   lastModified: new Date().toISOString(),
+    //   changeFrequency: "weekly",
+    //   priority: 0.8,
+    // })),
   ];
 }
 export async function dynamicSitemap() {
   try {
-    const request = await axios
-      .get(
-        `https://dev.to/api/articles?page=1&per_page=10`,
-        {
-          params: {
-            pageSize: 999,
-          },
-        }
-      )
-      .then((res:any) => res.data)
-      .catch((err:any) => console.log({ err }));
-    return request;
+    // const request = await axios
+    //   .get(
+    //     `https://dev.to/api/articles?page=1&per_page=10`,
+    //     {
+    //       params: {
+    //         pageSize: 999,
+    //       },
+    //     }
+    //   )
+    //   .then((res:any) => res.data)
+    //   .catch((err:any) => console.log({ err }));
+    // return request;
   } catch (error) {}
 }
